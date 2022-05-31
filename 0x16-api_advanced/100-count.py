c8874924222e60df7a8cb7836adea6a4ed8d3b73
cocofox1902 <colas.renard1902@gmail.com>
2022-05-31 11:41:07 +0000

    add pycodestyle

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -2,18 +2,19 @@
 """count words"""
 import requests
 
+
 def count_words(subreddit, word_list, nb_word={}, after=None):
     request = requests.get("https://www.reddit.com/r/{}/hot.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "holberton"},
-                            params={"after": after},
-                            allow_redirects=False)
+                           .format(subreddit),
+                           headers={"User-Agent": "holberton"},
+                           params={"after": after},
+                           allow_redirects=False)
 
     if request.status_code == 200:
         info = request.json()
 
         hot_lists = [k.get("data").get("title")
-                 for k in info.get("data").get("children")]
+                     for k in info.get("data").get("children")]
         if not hot_lists:
             return None
 
@@ -35,7 +36,8 @@ def count_words(subreddit, word_list, nb_word={}, after=None):
             sorted_counts = sorted(nb_word.items(),
                                    key=lambda kv: kv[1],
                                    reverse=True)
-            [print('{}: {}'.format(name, number)) for name, number in sorted_counts if number != 0]
+            [print('{}: {}'.format(name, number))
+             for name, number in sorted_counts if number != 0]
         else:
             return count_words(subreddit,
                                word_list,
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -12,13 +12,13 @@ def recurse(subreddit, hot_list=[], count=0, after=None):
 
     if request.status_code == 200:
         hot_lits = hot_list + [child.get("data").get("title")
-                        for child in request.json()
-                        .get("data")
-                        .get("children")]
+                               for child in request.json()
+                               .get("data")
+                               .get("children")]
         info = request.json()
         if not info.get("data").get("after"):
             return hot_lits
-        return recurse(subreddit, hot_lits, 
+        return recurse(subreddit, hot_lits,
                        info.get("data").get("count"),
                        info.get("data").get("after"))
     else:

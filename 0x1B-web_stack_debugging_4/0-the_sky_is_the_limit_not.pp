7e5a65230e5a08f2dbad1460829d4ba1987c6d6a
cocofox1902 <colas.renard1902@gmail.com>
2022-06-07 11:25:07 +0000

    add puppet

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+# testing how well our web server setup featuring Nginx
+exec { 'fix--for-nginx':
+  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
+}
